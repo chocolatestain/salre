dc03ca1770015de87a3323381dfd184f2ac57fa7
decoyer <nockback@naver.com>
2025-01-03 16:12:40 +0900

    250103

--- salre/src/main/java/com/salre/main/notification/NotificationController.java
+++ salre/src/main/java/com/salre/main/notification/NotificationController.java
@@ -1,13 +1,20 @@
 package com.salre.main.notification;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-@Controller
+import lombok.RequiredArgsConstructor;
+
+@RestController
 @RequestMapping("/notification")
+@RequiredArgsConstructor
 public class NotificationController {
 	@Autowired
 	NotificationService notificationService;
@@ -15,5 +22,15 @@ public class NotificationController {
 	@GetMapping("/main")
 	public ModelAndView viewMain() {
 		return new ModelAndView("notification/main");
+	}
+
+	@GetMapping(value = "/subscribe/{id}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public SseEmitter subscribe(@PathVariable Long id) {
+		return notificationService.subscribe(id);
+	}
+
+	@PostMapping("/send/{id}")
+	public void send(@PathVariable Long id) {
+		notificationService.notify(id, "data");
 	}
 }
