fcd167acbc484dd01ee43a414cbff265512e092e
decoyer <nockback@naver.com>
2025-01-03 12:28:14 +0900

    250103

--- salre/src/main/java/com/salre/main/product/HomeController.java
+++ salre/src/main/java/com/salre/main/product/HomeController.java
@@ -1,20 +1,18 @@
 package com.salre.main.product;
- 
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
- 
+
 @Slf4j
 @Controller
 public class HomeController {
@@ -22,16 +20,17 @@ public class HomeController {
     private ProductService productservice;
     @Autowired
     private RegionService regionservice;
+
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("productCount", productservice.countProduct());
-        model.addAttribute("regionCount", regionservice.countRegion()); 
+        model.addAttribute("regionCount", regionservice.countRegion());
         try {
-			model.addAttribute("regions", new ObjectMapper().writeValueAsString(regionservice.selectAllRegion()));
-		} catch (JsonProcessingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+            model.addAttribute("regions", new ObjectMapper().writeValueAsString(regionservice.selectAllRegion()));
+        } catch (JsonProcessingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         return "home";
     }
 
@@ -43,7 +42,7 @@ public class HomeController {
             return "search";
         }
 
-        log.info("검색어: {}", searchQuery);  // 로그로 검색어 확인
+        log.info("검색어: {}", searchQuery); // 로그로 검색어 확인
 
         // ProductService에서 검색 결과 가져오기
         List<ProductDTO> searchResults = productservice.searchProducts(searchQuery);
@@ -53,7 +52,7 @@ public class HomeController {
             model.addAttribute("message", "검색 결과가 없습니다.");
         } else {
             model.addAttribute("searchResults", searchResults);
-      
+
         }
 
         return "search";
